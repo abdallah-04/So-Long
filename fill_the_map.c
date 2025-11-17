1b517cb7c1ee68343c58e250f2395b668f93a4ca
Abdallah Mufleh <amufleh@c2r4s1.42amman.com>
2025-11-17 13:43:39 +0300

    add free map & fix valgrind

--- fill_the_map.c
+++ fill_the_map.c
@@ -6,34 +6,12 @@
 /*   By: amufleh <amufleh@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/05 11:40:56 by amufleh           #+#    #+#             */
-/*   Updated: 2025/11/10 15:47:03 by amufleh          ###   ########.fr       */
+/*   Updated: 2025/11/17 12:47:31 by amufleh          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-char	**fill_map(char *path, int lines)
-{
-	char	**map;
-	char	*str;
-	int		i;
-	int		fd;
-
-	if (!path)
-		return (0);
-	i = 0;
-	map = malloc((lines + 1) * sizeof(char *));
-	if (!map)
-		return (NULL);
-	fd = open(path, O_RDONLY);
-	if (fd == -1)
-		return (NULL);
-	while ((str = get_next_line(fd)) != NULL)
-		map[i++] = str;
-	map[i] = NULL;
-	close(fd);
-	return (map);
-}
 int	check_path(char *path)
 {
 	int		i;
@@ -57,4 +35,27 @@ int	check_path(char *path)
 		j++;
 	}
 	return (1);
+}
+
+char	**fill_map(char *path, int lines)
+{
+	char	**map;
+	char	*str;
+	int		i;
+	int		fd;
+
+	if (!path)
+		return (0);
+	i = 0;
+	map = malloc((lines + 1) * sizeof(char *));
+	if (!map)
+		return (NULL);
+	fd = open(path, O_RDONLY);
+	if (fd == -1)
+		return (NULL);
+	while ((str = get_next_line(fd)) != NULL)
+		map[i++] = str;
+	map[i] = NULL;
+	close(fd);
+	return (map);
 }
--- minilibx.c
+++ minilibx.c
@@ -6,7 +6,7 @@
 /*   By: amufleh <amufleh@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/08 15:03:26 by amufleh           #+#    #+#             */
-/*   Updated: 2025/11/17 11:17:29 by amufleh          ###   ########.fr       */
+/*   Updated: 2025/11/17 13:42:31 by amufleh          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@
 int is_there_c(char **map)
 {
 	int	i;
-	int j;
+	int	j;
 
 	i = 0;
 	while (map[i])
@@ -36,135 +36,155 @@ int is_there_c(char **map)
 
 void fill_image_map(t_game *game, int size)
 {
-    int i;
-    int j;
+	int	i;
+	int	j;
 
-    i = 0;
-    while (game->map[i])
-    {
-        j = 0;
-        while (game->map[i][j] && game->map[i][j] != '\n')
-        {
-            if (game->map[i][j] == '0' && (i + j) % 2 == 0)
-                mlx_put_image_to_window(game->mlx, game->mlx_win, game->textures->space1, j * size, i * size);
-            if (game->map[i][j] == '0' && (i + j) % 2 != 0)
-                mlx_put_image_to_window(game->mlx, game->mlx_win, game->textures->space2, j * size, i * size);
-            if (game->map[i][j] == 'P')
-                mlx_put_image_to_window(game->mlx, game->mlx_win, game->player->image, j * size, i * size);
-            if (game->map[i][j] == 'C')
-                mlx_put_image_to_window(game->mlx, game->mlx_win, game->textures->collectible, j * size, i * size);
-            if (game->map[i][j] == '1')
-                mlx_put_image_to_window(game->mlx, game->mlx_win, game->textures->wall, j * size, i * size);
-            if (game->map[i][j] == 'E')
-                mlx_put_image_to_window(game->mlx, game->mlx_win, game->textures->exit, j * size, i * size);
-            j++;
-        }
-        i++;
-    }
+	i = 0;
+	while (game->map[i])
+	{
+		j = 0;
+		while (game->map[i][j] && game->map[i][j] != '\n')
+		{
+			if (game->map[i][j] == '0' && (i + j) % 2 == 0)
+				mlx_put_image_to_window(game->mlx, game->mlx_win, game->textures->space1, j * size, i * size);
+			if (game->map[i][j] == '0' && (i + j) % 2 != 0)
+				mlx_put_image_to_window(game->mlx, game->mlx_win, game->textures->space2, j * size, i * size);
+			if (game->map[i][j] == 'P')
+				mlx_put_image_to_window(game->mlx, game->mlx_win, game->player->image, j * size, i * size);
+			if (game->map[i][j] == 'C')
+				mlx_put_image_to_window(game->mlx, game->mlx_win, game->textures->collectible, j * size, i * size);
+			if (game->map[i][j] == '1')
+				mlx_put_image_to_window(game->mlx, game->mlx_win, game->textures->wall, j * size, i * size);
+			if (game->map[i][j] == 'E')
+				mlx_put_image_to_window(game->mlx, game->mlx_win, game->textures->exit, j * size, i * size);
+			j++;
+		}
+		i++;
+	}
 }
 
 void swap_chars(char *c1, char *c2)
 {
-    char temp = *c1;
-    *c1 = *c2;
-    *c2 = temp;
+	char	temp;
+
+	temp = *c1;
+	*c1 = *c2;
+	*c2 = temp;
 }
+
 int move_player(t_game *game, char m)
 {
-    int new_x;
-    int new_y;
+	int	new_x;
+	int	new_y;
 
-    new_x = game->player->x_axis;
-    new_y = game->player->y_axis;
-    if (m == 'w')
-        new_x-= 1;
-    else if (m == 's')
-        new_x += 1;
-    else if (m == 'a')
-        new_y -= 1;
-    else if (m == 'd')
-        new_y += 1;
-    if (game->map[new_x][new_y] == '1')
-            return (0);
-    if (game->map[new_x][new_y] == 'C')
-        game->map[new_x][new_y] = '0';
-    if (game->map[new_x][new_y] == 'E')
-    {
-        if (!is_there_c(game->map))
-            return (1);
-        return (0);
-    }
-    swap_chars(&game->map[game->player->x_axis][game->player->y_axis], &game->map[new_x][new_y]);
-    game->player->x_axis = new_x;
-    game->player->y_axis = new_y;
-    return (0);
+	new_x = game->player->x_axis;
+	new_y = game->player->y_axis;
+	if (m == 'w')
+		new_x-= 1;
+	else if (m == 's')
+		new_x += 1;
+	else if (m == 'a')
+		new_y -= 1;
+	else if (m == 'd')
+		new_y += 1;
+	if (game->map[new_x][new_y] == '1')
+		return (0);
+	if (game->map[new_x][new_y] == 'C')
+		game->map[new_x][new_y] = '0';
+	if (game->map[new_x][new_y] == 'E')
+	{
+		if (!is_there_c(game->map))
+			return (1);
+		return (0);
+	}
+	swap_chars(&game->map[game->player->x_axis][game->player->y_axis], &game->map[new_x][new_y]);
+	game->player->x_axis = new_x;
+	game->player->y_axis = new_y;
+	return (0);
 }
 
 int handle_key(int keycode, t_game *game)
 {
-    int flag;
+	int	flag;
+	int static moves = 0;
 
-    flag = 0;
-    if (keycode == KEY_ESC)
-        exit(0);
-    else if (keycode == KEY_W)
-        flag = move_player(game, 'w');
-    else if (keycode == KEY_S)
-        flag = move_player(game, 's');
-    else if (keycode == KEY_A)
-        flag = move_player(game, 'a');
-    else if (keycode == KEY_D)
-        flag = move_player(game, 'd');
-    if (flag)
-        exit(0);
-    return (0);
+	flag = 0;
+	moves++;
+	if (keycode == KEY_ESC)
+		exit(0);
+	else if (keycode == KEY_W)
+		flag = move_player(game, 'w');
+	else if (keycode == KEY_S)
+		flag = move_player(game, 's');
+	else if (keycode == KEY_A)
+		flag = move_player(game, 'a');
+	else if (keycode == KEY_D)
+		flag = move_player(game, 'd');
+	if (flag)
+		exit(0);
+	printf("%d\n",moves);
+	return (0);
 }
-
 
 int fill_image(t_game *game)
 {
-    int width;
-    int height;
+	int	width;
+	int	height;
 
-    if (! game)
-        return (0);
-    game->textures->wall = mlx_xpm_file_to_image(game->mlx, "textures/wall3.xpm", &width, &height);
-    game->textures->space1 = mlx_xpm_file_to_image(game->mlx, "textures/space1.xpm", &width, &height);
-    game->textures->space2 = mlx_xpm_file_to_image(game->mlx, "textures/space2.xpm", &width, &height);
-    game->textures->collectible = mlx_xpm_file_to_image(game->mlx, "textures/collectible2_b.xpm", &width, &height);
-    game->textures->exit = mlx_xpm_file_to_image(game->mlx, "textures/exit2.xpm", &width, &height);
-    game->player->image = mlx_xpm_file_to_image(game->mlx, "textures/player2.xpm", &width, &height);
-    if (!game->textures->wall || !game->textures->space1 || !game->textures->space2 ||
-        !game->textures->collectible || !game->textures->exit || !game->player->image)
-        return (0);
-    return (1);
+	if (! game)
+		return (0);
+	game->textures->wall = mlx_xpm_file_to_image(game->mlx, "textures/wall3.xpm", &width, &height);
+	game->textures->space1 = mlx_xpm_file_to_image(game->mlx, "textures/space1.xpm", &width, &height);
+	game->textures->space2 = mlx_xpm_file_to_image(game->mlx, "textures/space2.xpm", &width, &height);
+	game->textures->collectible = mlx_xpm_file_to_image(game->mlx, "textures/collectible2_b.xpm", &width, &height);
+	game->textures->exit = mlx_xpm_file_to_image(game->mlx, "textures/exit2.xpm", &width, &height);
+	game->player->image = mlx_xpm_file_to_image(game->mlx, "textures/player2.xpm", &width, &height);
+	if (!game->textures->wall || !game->textures->space1 || !game->textures->space2 ||
+			!game->textures->collectible || !game->textures->exit || !game->player->image)
+		return (0);
+	return (1);
 }
 
 int render_next_frame(t_game *game)
 {
-    mlx_clear_window(game->mlx, game->mlx_win);
-    fill_image_map(game, 52);
-    return (0);
+	mlx_clear_window(game->mlx, game->mlx_win);
+	fill_image_map(game, 52);
+	return (0);
 }
 int count_line(char *path)
 {
-    int line;
-    int fd;
-    char *str;
-    if (! path)
-        return (0);
+	int	line;
+	int	fd;
+	char	*str;
+
+	if (! path)
+		return (0);
 	fd = open(path, O_RDONLY);
 	if (fd == -1)
 		perror("open");
-    line = 0;
+	line = 0;
 	while ((str = get_next_line(fd)) != NULL)
 	{
 		line++;
 		free(str);
 	}
-    close(fd);
-    return (line);
+	close(fd);
+	return (line);
 }
+
+void free_map(char **map)
+{
+	int	i;
+
+	i = 0;
+	while (map[i])
+	{
+		free(map[i]);
+		i++;
+	}
+	free(map);
+}
+
 int main()
 {
     //cc *.c get_next_line/getnextline.a -lmlx -lXext -lX11
@@ -179,28 +199,38 @@ int main()
 
 
     path = "test.ber";
+    if (!check_path(path))
+    {
+        printf("Error\nThe path is !valid\n");
+        return (0);
+    }
     lines = count_line(path);
     game.map = fill_map(path,lines);
     temp = fill_map(path, lines);
 	if (!is_valid(temp, lines))
-        printf("\nerror ->\n");
+    {
+        free_map(game.map);
+        free_map(temp);
+        printf("Error\nThe map is !valid\n");
+        return (0);
+    }
     rows = count_line (path);
     cols = ft_strlen (game.map[0]) - 1;
     game.mlx = mlx_init();
     if (!game.mlx)
         return (0);
     game.mlx_win = mlx_new_window(game.mlx, cols * 52, rows * 52, "so_long");
+    if (!game.mlx_win)
+        return (0);
     game.player = &player;
     game.textures = &textures;
     fill_image(&game);
     find_player(game.map, &player.x_axis, &player.y_axis);
     fill_image_map(&game, 52);
     mlx_key_hook(game.mlx_win, handle_key, &game);
+    //mlx_mouse_hook(game.mlx_win, mouse_hook, &vars);
     mlx_loop_hook(game.mlx, render_next_frame, &game);
-    printf("%d",player.y_axis);
     mlx_loop(game.mlx);
 
     return (0);
 }
-
-
